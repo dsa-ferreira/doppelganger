5dc4490c4e741b0b4724595146a8ea4460d6a268
Teach <fdiogo@protonmail.com>
2025-03-13 00:52:06 +0000

    makes code and verb optional

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,6 +16,35 @@ type Mapping struct {
 	Content  any    `json:"content"`
 }
 
+func (mapping *Mapping) UnmarshalJSON(data []byte) error {
+	type Alias Mapping
+	type Aux struct {
+		RespCode *int    `json:"append"` // We override the type of Append
+		Verb     *string `json:"verb"`
+		*Alias
+	}
+	aux := &Aux{Alias: (*Alias)(mapping)}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	if aux.RespCode == nil {
+		// Field "append" is not set: we want the default value to be true.
+		mapping.RespCode = 204
+	} else {
+		// Field "append" is set: dereference and assign the value.
+		mapping.RespCode = *aux.RespCode
+	}
+
+	if aux.Verb == nil {
+		mapping.Verb = "GET"
+	} else {
+		mapping.Verb = *aux.Verb
+	}
+
+	return nil
+}
 func ParseConfiguration(filePath string) (*Configuration, error) {
 	file, err := readFile(filePath)
 	if err != nil {
